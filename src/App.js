a1d18bea8e6d9967f6291e88e8f4610880ad4685
Louis Williams <louiswilliams@Louiss-MacBook-Pro.local>
2023-11-25 23:49:49 -0500

    added travis file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey< code>src/App.js</code> and save to reload.
+          Go to <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
